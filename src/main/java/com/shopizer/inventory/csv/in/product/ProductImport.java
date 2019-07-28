8b3555cb03fb6baeb3df52c1b5ee778091257992
shopizer-ecommerce <csamson777@yahoo.com>
2019-07-28 18:38:05 -0400

    Import improvement

--- src/main/java/com/shopizer/inventory/csv/in/product/ProductImport.java
+++ src/main/java/com/shopizer/inventory/csv/in/product/ProductImport.java
@@ -27,25 +27,77 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.codec.Base64;
 import org.springframework.web.client.RestTemplate;
 
-import com.salesmanager.web.entity.catalog.category.Category;
-import com.salesmanager.web.entity.catalog.manufacturer.Manufacturer;
-import com.salesmanager.web.entity.catalog.product.PersistableImage;
-import com.salesmanager.web.entity.catalog.product.PersistableProduct;
-import com.salesmanager.web.entity.catalog.product.PersistableProductPrice;
-import com.salesmanager.web.entity.catalog.product.ProductDescription;
+import com.salesmanager.shop.model.catalog.category.Category;
+import com.salesmanager.shop.model.catalog.product.PersistableImage;
+import com.salesmanager.shop.model.catalog.product.PersistableProduct;
+import com.salesmanager.shop.model.catalog.product.PersistableProductPrice;
+import com.salesmanager.shop.model.catalog.product.ProductDescription;
+import com.salesmanager.shop.model.catalog.product.ProductSpecification;
+import com.salesmanager.shop.store.security.AuthenticationRequest;
+import com.salesmanager.shop.store.security.AuthenticationResponse;
+
 
 public class ProductImport {
 	
 	private Map<String,Integer> skuMap = new HashMap<String,Integer>();
 	
-	//private String IMAGE_BASE_DIR = "/Users/carlsamson/Documents/csti/mobilia-exotika/29-11-2015-resized-600W/JPEG/";
-	private String IMAGE_BASE_DIR = "/Users/carlsamson/Documents/csti/mobilia-exotika/pictures-600-resize/";
+	/**
+	 * Fields used in xls file with Product mapping
+	 * 
+	 * barcode
+	 * sku
+	 * image
+	 * image_file
+	 * name_<lang>
+	 * description_<lang>
+	 * quantity
+	 * collection
+	 * category
+	 * height
+	 * width
+	 * length
+	 * weight
+	 * price
+	 * deal -> % discounted
+	 * pre-order
+	 * dimensions -> CM | IN (centimeters or inches)
+	 * position -> placement in listing page (ordering)
+	 * import -> yes | no
+	 * 
+	 * NEED JAVA 1.8
+	 */
 	
-									
-	private String FILE_NAME = "/Users/carlsamson/Documents/dev/workspaces/shopizer-inventoty-xls/shopizer-inventory-csv/src/main/resources/products_20.csv";
+	/**
+	 * Supported languages
+	 * CSV template must contain name_<language> and description<language>
+	 */
+	private String langs[] = {"en","fr"};
+	
+	private HttpHeaders httpHeader;
+	
+	//private String baseUrl = "http://ec2-35-182-159-217.ca-central-1.compute.amazonaws.com:8080";
+	private String baseUrl = "http://localhost:8080";
+	
+	private boolean DRY_RUN = false;
+	
+	private final int MAX_COUNT = 21;
+	
+	
+	
+	/**
+	 * Where to find images
+	 */
+	private String IMAGE_BASE_DIR = "/Users/carlsamson/Documents/csti/projects-proposals/bam/BAM-excel/";
+	
+	/**
+	 * where to find csv							
+	 */
+	//private String FILE_NAME = "/Users/carlsamson/Documents/dev/workspaces/shopizer-tools/tools/src/main/resources/2017-01-25.csv";
+	//private String FILE_NAME = "/Users/carlsamson/Documents/csti/shopizer/Revamp-2.5/Shopizer-demo/products-import.csv";
+	private String FILE_NAME = "/Users/carlsamson/Documents/csti/projects-proposals/bam/BAM-excel/BAM-import_list_02.csv";
+	//private String FILE_NAME = "/Users/carlsamson/Documents/dev/workspaces/shopizer-tools/tools/src/main/resources/BAM-import.csv";
 	
 	private int iterationCount = 1;
 
@@ -62,18 +114,29 @@ public class ProductImport {
 	
 	public void importProducts() throws Exception {
 		
+		
+		
 		RestTemplate restTemplate = new RestTemplate();
 		
 		CSVFormat format = CSVFormat.EXCEL.withHeader().withDelimiter(',');
 		
+/*		FileInputStream is = new FileInputStream(FILE_NAME);
+		InputStreamReader isr = new InputStreamReader(is,StandardCharsets.UTF_8);
+		int data = isr.read();
+	    while(data != -1) {
+	        System.out.print((char) data);
+	        data = isr.read();
+	    }*/
 		
-
+		
+		
 		BufferedReader in = new BufferedReader(
 				   new InputStreamReader(
-		                      new FileInputStream(FILE_NAME), StandardCharsets.ISO_8859_1));
+		                      new FileInputStream(FILE_NAME), StandardCharsets.UTF_8));
 		
 		@SuppressWarnings("resource")
 		CSVParser parser = new CSVParser(in,format);
+
 		
 		HttpHeaders httpHeader = getHeader();
 
@@ -87,29 +150,28 @@ public class ProductImport {
 				System.out.println("Skipping code " + i);
 				i++;
 				continue;
-			} else {
-				//generateSku();
-			}
+			} 
 			
 			String barcode = record.get("barcode");
 
 			if(StringUtils.isBlank(barcode)) {
-				System.out.println("Skipping barcode " + i);
-				i++;
-				continue;
-			} else {
-				//generateSku();
+				barcode = this.alternativeIdentifier(record);
+				if(StringUtils.isBlank(barcode)) {
+					System.out.println("Skipping barcode " + i);
+					i++;
+					continue;
+				}
+
 			}
 			
-			String imp = record.get("import");
+			String imp = record.get("import");//import this row ? yes|no
+			System.out.println("Import status [" + imp + "]");
 			
 			if(StringUtils.isBlank(imp) || "no".equals(imp)) {
 				System.out.println("Skipping import " + i);
 				i++;
 				continue;
-			} else {
-				//generateSku();
-			}
+			} 
 			
 
 			code = uniqueSku(code);
@@ -142,34 +204,39 @@ public class ProductImport {
 			}
 			int quantity = Integer.parseInt(stringQuantity);
 			
+			price = price.replaceAll(",", ".").trim();
+			
 			BigDecimal productPrice = new BigDecimal(price);
 			
 			Category category = new Category();
 			category.setCode(categoryCode);
 			List<Category> categories = new ArrayList<Category>();
 			categories.add(category);
+
 			
+			/** specification **/
+			String manufacturer = record.get("collection");//brand - manufacturer ...
+			ProductSpecification specs = new ProductSpecification();
+			specs.setManufacturer(manufacturer);
 			
-/*			System.out.println(code);
-			System.out.println(record.get("name_en"));
-			System.out.println(record.get("name_fr"));
-			System.out.println(record.get("title_en"));
-			System.out.println(record.get("title_fr"));
-			System.out.println(record.get("friendlyUrl_en"));
-			System.out.println(record.get("friendlyUrl_fr"));
-			System.out.println(record.get("position"));
-			System.out.println(record.get("visible"));
-			System.out.println(record.get("parent"));*/
+			//sizes are required when loading a product
+			String dimensions = record.get("dimensions");
 			
-			String manufacturer = record.get("collection");
-			Manufacturer collection = new Manufacturer();
-			collection.setCode(manufacturer);
+			String W = convertDimension(record.get("width"),dimensions).toString();
+			String L = convertDimension(record.get("length"),dimensions).toString();
+			String H = convertDimension(record.get("length"),dimensions).toString();
+			
+			specs.setHeight(convertDimension(record.get("height"),dimensions));
+			specs.setWidth(convertDimension(record.get("width"),dimensions));
+			specs.setLength(convertDimension(record.get("length"),dimensions));
+			specs.setWeight(convertWeight(record.get("weight")));
+			//specs.setDimensionUnitOfMeasure(dimensionUnitOfMeasure);
 			
 			//core properties
 			PersistableProduct product = new PersistableProduct();
 			product.setSku(code);
 			product.setRefSku(barcode);
-			product.setManufacturer(collection);
+			product.setProductSpecifications(specs);
 			product.setCategories(categories);
 			
 			product.setSortOrder(order);//set the order or iterator as sort order
@@ -179,38 +246,32 @@ public class ProductImport {
 			product.setProductShipeable(true);//all items are shipeable
 			
 			/** images **/
-			String image = record.get("image_file");
+			String image = record.get("image_name");
 			if(!StringUtils.isBlank(image)) {
 				
 				StringBuilder imageName = new StringBuilder();
-				imageName.append(IMAGE_BASE_DIR).append(image);
+				imageName.append(IMAGE_BASE_DIR).append(image.trim()).append(".jpg");//TODO remove
 				
 				File imgPath = new File(imageName.toString());
 				
-				PersistableImage persistableImage = new PersistableImage();
-				
-				
-				persistableImage.setBytes(this.extractBytes(imgPath));
-				persistableImage.setImageName(imgPath.getName());
-
-				List<PersistableImage> images = new ArrayList<PersistableImage>();
-				images.add(persistableImage);
-				
-				product.setImages(images);
+				byte[] bytes = this.extractBytes(imgPath);
+				if(bytes!=null) {
+					PersistableImage persistableImage = new PersistableImage();
+						
+						
+					persistableImage.setBytes(bytes);
+					persistableImage.setName(imgPath.getName());
+		
+					List<PersistableImage> images = new ArrayList<PersistableImage>();
+					images.add(persistableImage);
+						
+					product.setImages(images);
+				}
 				
 			}
 
-			
-			String dimensions = record.get("dimensions");
 
-			product.setProductHeight(convertDimension(record.get("height"),dimensions));
-			product.setProductWidth(convertDimension(record.get("width"),dimensions));
-			product.setProductLength(convertDimension(record.get("length"),dimensions));
-
-
-			product.setProductWeight(convertWeight(record.get("weight")));
-			
-
+		
 			product.setQuantity(quantity);
 			product.setQuantityOrderMaximum(maximumQuantity(quantity));
 			product.setQuantityOrderMinimum(minimumQuantity(quantity));
@@ -221,10 +282,12 @@ public class ProductImport {
 			persistableProductPrice.setOriginalPrice(productPrice);
 			
 			//apply a discunted price
-			String discount = record.get("deal");
-			BigDecimal discountedPrice = this.createDiscountedPrice(productPrice, discount);
-			if(discountedPrice != null) {
-				persistableProductPrice.setDiscountedPrice(discountedPrice);
+			if(record.isSet("deal")) {
+				String discount = record.get("deal");
+				BigDecimal discountedPrice = this.createDiscountedPrice(productPrice, discount);
+				if(discountedPrice != null) {
+					persistableProductPrice.setDiscountedPrice(discountedPrice);
+				}
 			}
 			
 			List<PersistableProductPrice> productPriceList = new ArrayList<PersistableProductPrice>();
@@ -234,78 +297,86 @@ public class ProductImport {
 
 			List<ProductDescription> descriptions = new ArrayList<ProductDescription>();
 
-			//add english description
-			
-/*			ProductDescription description = new ProductDescription();
-			description.setLanguage("en");
-			//description.setTitle(record.get("title_en"));
-			description.setTitle(record.get("name_en"));
-			description.setName(record.get("name_en"));
-			description.setDescription(record.get("description_en"));
-			//description.setFriendlyUrl(record.get("friendlyUrl_en"));
-			description.setFriendlyUrl(minimalFriendlyUrlCreator(description.getName()));
-			//description.setHighlights(record.get("highlights_en"));
-			
-			descriptions.add(description);*/
-			
-			ProductDescription description = new ProductDescription();
-			
-			//add french description
-			description = new ProductDescription();
-			description.setLanguage("fr");
-			description.setTitle(record.get("name_fr") + " - Exotik Mobilier");
-			description.setName(record.get("name_fr"));
-			description.setDescription(record.get("description_fr"));
-			description.setFriendlyUrl(minimalFriendlyUrlCreator(description.getName()));
-			//description.setHighlights(record.get("highlights_fr"));
-			
-			descriptions.add(description);
+						
+			for(int langLenth=0;langLenth<langs.length;langLenth++) {
+				
+				ProductDescription description = new ProductDescription();
+				
+				String lang = langs[langLenth];
+				
+				if(!record.isSet("pre") || !StringUtils.isBlank(record.get("pre-order"))) {
+					//something specific must be written ?
+				}
+
+				
+				description = new ProductDescription();
+				description.setLanguage(lang);
+				description.setTitle(record.get("name_" + lang));
+				description.setName(record.get("name_" + lang));
+				description.setDescription(record.get("description_" + lang));
+				if(StringUtils.isBlank(description.getName())) {
+					description.setName(description.getDescription());
+				}
+				description.setFriendlyUrl(minimalFriendlyUrlCreator(description.getName()));
+				descriptions.add(description);
+				
+			}
 
 			product.setDescriptions(descriptions);
-			
-			
+
 			ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
 			String json = writer.writeValueAsString(product);
 			
-			//System.out.println(json);
-			
-			
-			HttpEntity<String> entity = new HttpEntity<String>(json, httpHeader);
-
-			//post to create category web service
-			ResponseEntity response = restTemplate.postForEntity("http://www.exotikmobilier.com/services/private/DEFAULT/product", entity, PersistableProduct.class);
-			PersistableProduct prod = (PersistableProduct) response.getBody();
-			
+			System.out.println("Line " + i + " ********************");
+			System.out.println(json);
 
 			
-			//System.out.println("--------------------- " + i);
+			
+
+			//post to create category v0 API web service
+
+			if(!DRY_RUN) {
+				HttpEntity<String> entity = new HttpEntity<String>(json, httpHeader);
+				ResponseEntity response = restTemplate.postForEntity(baseUrl + "/api/v1/private/product", entity, PersistableProduct.class);
+				PersistableProduct prod = (PersistableProduct) response.getBody();
+			}
+			
+			System.out.println("Created product in row " + i + " Dry Run [" + DRY_RUN + "]");
+
 			i++;//rows
 			count++;
 			
-			//if(i==30) {
-			//	break;
-			//}
+			if(i==MAX_COUNT) {
+				System.out.println("Reached MAX_COUNT [" + MAX_COUNT + "]");
+				break;
+			}
 		}
 		
 		
 		System.out.println("------------------------------------");
-		System.out.println("Product import done " + count);
+		System.out.println("Product import done " + count + " Dry Run [" + DRY_RUN + "]");
 		System.out.println("------------------------------------");
 		
 	}
 	
 	
-	private HttpHeaders getHeader(){
-		HttpHeaders headers = new HttpHeaders();
-		MediaType mediaType = new MediaType("application", "json", Charset.forName("UTF-8"));
-		//MediaType.APPLICATION_JSON //for application/json
-		headers.setContentType(mediaType);
-		//Basic Authentication
-		String authorisation = "admin" + ":" + "Montreal2016!";
-		byte[] encodedAuthorisation = Base64.encode(authorisation.getBytes());
-		headers.add("Authorization", "Basic " + new String(encodedAuthorisation));
-		return headers;
-	}
+    protected HttpHeaders getHeader() {
+    	if (DRY_RUN) return null;
+    	if(this.httpHeader != null) {
+    		return this.httpHeader;
+    	}
+    	RestTemplate restTemplate = new RestTemplate();
+        final ResponseEntity<AuthenticationResponse> response = restTemplate.postForEntity(baseUrl +"/api/v1/private/login", new HttpEntity<>(new AuthenticationRequest("admin", "password")),
+                AuthenticationResponse.class);
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+        headers.add("Authorization", "Bearer " + response.getBody().getToken());
+        this.httpHeader = headers;
+        return this.httpHeader;
+    }
+
+	
+
 	
 	
 	/**
@@ -334,12 +405,17 @@ public class ProductImport {
 			skuMap.put(sku, iterationCount);
 		}
 		
-		return sku;
+		return sku.trim();
 	}
 	
 	private BigDecimal convertDimension(String value, String dimensions) {
 		
 		//in our case we convert from cm to inches 1 * 0.393701
+		if(StringUtils.isBlank(value)) {
+			value = "0";
+		}
+		value=value.replaceAll(",",".").trim();
+		//System.out.println("Dimension " + value);
 		BigDecimal decimalValue = new BigDecimal(value);
 		
 		if(StringUtils.isBlank(dimensions) || "CM".equals(dimensions)) {
@@ -354,6 +430,10 @@ public class ProductImport {
 	private BigDecimal convertWeight(String value) {
 		
 		//in our case we already have the good weight
+		if(StringUtils.isBlank(value)) {
+			value = "0";
+		}
+		value=value.replaceAll(",",".").trim();
 		BigDecimal decimalValue = new BigDecimal(value);
 		BigDecimal scaled = decimalValue.setScale(0, RoundingMode.HALF_UP);
 		//decimalValue = decimalValue.multiply(new BigDecimal("0.393701"));
@@ -363,19 +443,12 @@ public class ProductImport {
 	
 	private BigDecimal createDiscountedPrice(BigDecimal price, String discount) {
 		
-		if(StringUtils.isNotBlank(discount)) {
-			if("50".equals(discount)) {
-				price = price.multiply(new BigDecimal(0.50));
-			}
-			if("30".equals(discount)) {
-				price = price.multiply(new BigDecimal(0.70));
-			}
-
-			
+		if(StringUtils.isNotBlank(discount) && !discount.equals("0")) {
+			double percent = 1 - Double.parseDouble(discount) / 100d;
+			price = price.multiply(new BigDecimal(percent));
 			return roundAmount(price);
 		}
-		
-		
+
 		return null;
 		
 	}
@@ -386,6 +459,14 @@ public class ProductImport {
 	}
 	
 	public byte[] extractBytes (File imgPath) throws Exception {
+		
+		if(!imgPath.exists()) {
+			System.out.println("--------------------------------------");
+			System.out.println("IMAGE NOT FOUND " + imgPath.getName());
+			System.out.println("IMAGE PATH " + imgPath.getAbsolutePath());
+			System.out.println("--------------------------------------");
+			return null;
+		}
 		
 		FileInputStream fis = null;
 		BufferedInputStream inputStream = null;
@@ -447,6 +528,10 @@ public class ProductImport {
 	public int minimumQuantity(int quantity) {
 
 		return Integer.parseInt("1");
+	}
+	
+	private String alternativeIdentifier(CSVRecord record) {
+		return record.get("sku");
 	}
 
 }
